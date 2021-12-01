ef9e7daa74326bf6a865e443b5e03deaa3013708
seyhadot <seyhadot@gmail.com>
2021-12-01 23:39:24 +0700

    setup dotfile

--- installer.sh
+++ installer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+brew tap mongodb/brew
 
-brew install curl git fish tmux neovim python3 nodejs yarn wget fish 
+brew install curl git fish tmux neovim python3 nodejs yarn wget fish mongodb-community@5.0
 
 sudo spctl --master-disable
+sh -c "$(curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher)"
 
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
@@ -14,12 +16,8 @@ sh -c "$(curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin
 sh -c "$(curl -sSL https://get.rvm.io | bash -s stable --ruby)"
 sh -c "$(curl -L --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish)"
 echo 'rvm default' >> ~/.config/fish/config.fish
-# git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
 
-# curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-# curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-
-git clone git://github.com/seyhadot/config.git ~/.dotfiles
+git clone git://github.com:seyhadot/config.git ~/.dotfiles
 
 cd ~/.dotfiles && brew bundle
 
