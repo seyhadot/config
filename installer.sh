340adc156be128538593cba4d50b503c3cd7d661
seyhadot <seyhadot@gmail.com>
2021-12-01 23:28:01 +0700

    setup dotfile

---
+++ installer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+brew install curl git fish tmux neovim python3 nodejs yarn wget fish 
+
+sudo spctl --master-disable
+
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+sh -c "$(curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
+sh -c "$(curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish)"
+sh -c "$(curl -sSL https://get.rvm.io | bash -s stable --ruby)"
+sh -c "$(curl -L --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish)"
+echo 'rvm default' >> ~/.config/fish/config.fish
+# git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+
+# curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+# curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+git clone git://github.com/seyhadot/config.git ~/.dotfiles
+
+cd ~/.dotfiles && brew bundle
+
+ln -f -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/aliases ~/.aliases
+
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/irbrc ~/.irbrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/ctags ~/.ctags
+ln -s ~/.dotfiles/ackrc ~/.ackrc
+ln -s ~/.dotfiles/agignore ~/.agignore
+ln -s ~/.dotfiles/gitmessage ~/.gitmessage
+ln -s ~/.dotfiles/starship.toml ~/.starship.toml
+
+vim +PlugInstall +qall
---
+++ omf.fish
@@ -0,0 +1,55 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+or set -gx OMF_PATH "$HOME/.local/share/omf"
+set fish_greeting
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+# set PATH $HOME/.rbenv/bin $PATH
+# set PATH $HOME/.rbenv/shims $PATH
+
+# set --export (pwd)/flutter/bin $PATH
+set -gx PATH $PATH $HOME/.deno/bin
+set -gx PATH $PATH $HOME/flutter/bin
+set -gx PATH $PATH $HOME/flutter/.pub-cache/bin
+
+set -gx PATH $PATH $HOME/.pub-cache/bin
+
+# set -gx PATH="$PATH":"$HOME/.pub-cache/bin"
+
+set --export ANDROID $HOME/Library/Android;
+
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+#set -gx PATH $ANDROID_HOME/usr/local/opt/android-sdk $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+
+function mvim
+  open -a 'macvim' .
+end
+
+function web
+  open -a /Applications/WebStorm.app .
+end
+
+function dk
+  cd ~/Desktop
+end
+
+function dg
+  cd ~/Documents/GitHub
+end
+
+function serve
+  yarn serve
+end
+
+function gpull
+  git pull origin master
+end
+
+function gpush
+  git push origin 
+end
